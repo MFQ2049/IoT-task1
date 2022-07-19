bf05f23402c4074607c729b8ab350c0955b5b9ed
Mohammed Faisal <75149379+MFQ2049@users.noreply.github.com>
2022-07-19 17:58:04 +0300

    Add files via upload

---
+++ ESP32-Arduino/Blink/Blink.ino
@@ -0,0 +1,11 @@
+//Make sure to read the instructions file before you run the code
+int LED_BUILTIN = 2;
+void setup() {
+pinMode (LED_BUILTIN, OUTPUT);
+}
+void loop() {
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1000);
+digitalWrite(LED_BUILTIN, LOW);
+delay(1000);
+}
